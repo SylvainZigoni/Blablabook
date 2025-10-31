257499c99c3903d4450f57c244ad5c00fc79701c
student <ced.campagne@gmail.com>
2025-10-31 14:39:45 +0100

    composant Header page accueil

---
+++ client/src/lib/components/layout/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import Icon  from '@iconify/svelte';
+
+    let currentPath;
+    // Réactivité automatique avec $:
+    $: currentPath = $page.url.pathname
+
+</script>
+
+<header>
+    <div class="header_title">
+        <h1>BlaBlaBook</h1>
+        <Icon icon="ion:book" height = 50 width= 50 />
+    </div>
+    {#if currentPath === '/page-user'}
+        <!-- a migrer en composant pour afficher selon la route front 
+             il est sur les routes user, search,book, admin
+        -->
+        <div class="header_search">
+            <input type="text">
+            <input id="author" type="radio">
+            <label for="author"></label>
+            <input type="radio">
+        </div>  
+    {/if}
+    {#if currentPath === '/'}
+    <button on:click={()=> goto('page-log')}>S'inscrire</button>
+    <button on:click={()=> goto('page-log')}>Se Connecter</button>
+    {/if}
+</header>
+
+
+<style>
+    header{
+        border: 1px solid black;
+        color: #fff;
+        background-color: var(--color-header-footer);
+        padding: 1rem;
+    }
+    .header_title{
+        display: flex;
+        align-items: center;  
+    }
+
+    h1{
+        margin-right: 1rem;
+    }
+    button{
+        padding: 0.5rem;
+        background-color: #CAD2C5;
+        color: var(--color-text-main);
+    }
+</style>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+    import Header from '$lib/components/layout/Header.svelte';
+	import '../app.css';
+	
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<Header/>
+<slot/>
 
-{@render children?.()}
+
