d7d60933cc39fdadfbf67e0b2789597655dd06ec
student <ced.campagne@gmail.com>
2025-10-31 19:10:01 +0100

    feat ajout du composant Footer au layout principal + line vers la route mentions légales + page mention légales a completer (contenu et mis en page css)

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -3,11 +3,44 @@
     import Header from '$lib/components/layout/Header.svelte';
 	import '../styles/reset.css';
 	import '../styles/app.css';
-	
-	
+    import Footer from '$lib/components/layout/Footer.svelte';
 </script>
+<div class="layout">
+	<Header/>
+	<main>	
+		<slot/>
+	</main>
+	<Footer/>
+</div>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden; /* empêche le scroll global */
+}
 
-<Header/>
-<slot/>
+.layout {
+  height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto; /* header, slot, footer */
+}
 
+header {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+}
 
+main {
+  overflow-y: auto;
+  padding: 1rem;
+}
+
+footer {
+  position: sticky;
+  bottom: 0;
+  z-index: 10;
+}
+
+</style>
