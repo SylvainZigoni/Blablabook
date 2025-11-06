46678dd4456df48880075a97a45ed72c160b7b6f
SylvainZigoni <sylvain.zigoni@oclock.school>
2025-11-06 17:52:33 +0100

    Feat: Ajout commentaires explicatifs

--- client/src/lib/components/ui/Search.svelte
+++ client/src/lib/components/ui/Search.svelte
@@ -16,16 +16,24 @@
         error = '';
 
         try {
+            // On définie la route Svelte ou le fetch va etre executer et on lui fourni les paramètre query et by
             const response = await fetch(`/api/search?${params.toString()}`);
+
+            // Si on arrive pas a connecter avec le fichier api/search/+server.js alors: 
             if (!response.ok) throw new Error (await response.text() || response.statusText);
+            
+            // On enregistre nos résultats
             results = await response.json();
             console.log(results)
 
+            // On modifie l'url de notre navigateur à partir de la ou on est.
+            // url.search permet d'ajouter "?q=<texte cherché>&by=<author ou title>"
             const url = new URL(window.location.href);
             url.search = params.toString();
-
+        
+        
         } catch (error) {
-            error = error.message;
+            console.error("Erreur :", error); 
             results = [];
 
         }
@@ -34,6 +42,7 @@
 
 
 <div class="header_search">
+    <!-- On ajoute la notion de preventDefault directement dans le form -->
     <form on:submit|preventDefault={handleSearch}>
         <input type="text" placeholder="Recherche..." name="content" bind:value={query}/>
             <div>
--- client/src/routes/api/search/+server.js
+++ client/src/routes/api/search/+server.js
@@ -1,9 +1,16 @@
+// Import de l'utilitaire json pour ne pas avoir a renvoyer une reponse http, mais seulement un objet
+import { json } from "@sveltejs/kit";
+
 export async function GET({ url }) {
+	// On récupère les paramètres d'url
 	const query = url.searchParams.get("q");
 	const by = url.searchParams.get("by");
 
 	console.log(query, by);
+	// TODO : Mise en place du try catch
 
+	// On réalise notre fetch
+	// TODO : Ajouter le bearer + token avec la route définie coté back
 	const response = await fetch(
 		`${import.meta.env.VITE_API_BASE_URL}/books/random`
 	);
@@ -12,7 +19,5 @@ export async function GET({ url }) {
 	console.log(data);
 	console.log(JSON.stringify(data));
 
-	return new Response(JSON.stringify(data), {
-		headers: { "Content-Type": "application/json" },
-	});
+	return json(data);
 }
