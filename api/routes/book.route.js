c9a3f55f3e2ecaac910dff59a7161c1fcb4dc511
FedericaZucchini <federica.zucchini@oclock.school>
2025-11-07 09:50:11 +0100

    feat : add routes for searching books by title and author

--- api/routes/book.route.js
+++ api/routes/book.route.js
@@ -6,5 +6,7 @@ const bookRouter = Router();
 
 bookRouter.get("/random", bookController.getRandomBooks);
 bookRouter.get("/:userId", isAuthed, bookController.getAllUserBooks);
+bookRouter.get("/title/:titleSearched", isAuthed, bookController.getBooksByTitle);
+bookRouter.get("/author/:authorSearched", isAuthed, bookController.getBookByAuthor);
 
 export default bookRouter;
