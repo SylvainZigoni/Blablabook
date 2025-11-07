78c0be9f6a87a0d50568a883599d11d66b2ef2eb
Valentin Colombat <valentincolombat@gmail.com>
2025-11-07 15:19:06 +0100

    s

--- api/routes/book.route.js
+++ api/routes/book.route.js
@@ -6,7 +6,7 @@ const bookRouter = Router();
 
 bookRouter.get("/random", bookController.getRandomBooks);
 bookRouter.get("/title/:titleSearched",isAuthed, bookController.getBooksByTitle);
-bookRouter.get("/author/:authorSearched",isAuthed, bookController.getBookByAuthor);
+bookRouter.get("/author/:authorSearched",isAuthed, bookController.getBooksByAuthor);
 bookRouter.delete("/:userId/:bookId",isAuthed, bookController.deleteUserBook);
 bookRouter.post("/:userId/:bookId",isAuthed, bookController.addUserBook);
 bookRouter.get("/:userId",isAuthed, bookController.getAllUserBooks);
